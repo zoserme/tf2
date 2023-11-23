e14cf3a59a267f74280ce090ad63be4e797e149e
zoserme <149444912+zoserme@users.noreply.github.com>
2023-11-22 22:05:06 -0500

    Add files via upload

--- 20231120_trabfinaiapcd.py
+++ 20231120_trabfinaiapcd.py
@@ -89,13 +89,13 @@ df_piura = df_final['DISTRITO'].unique()
 #    ).add_to(piura_map)
 
 # Mostrar el mapa
-piura_map
+#piura_map
 
 # Crear el dashboard
-st.title("Dashboard del Departamento de Piura")
-st.markdown("Mapa en dos dimensiones con los distritos del departamento de Piura")
+#st.title("Dashboard del Departamento de Piura")
+#st.markdown("Mapa en dos dimensiones con los distritos del departamento de Piura")
 # Mostrar el mapa en el dashboard
-st.write(piura_map._repr_html_(), unsafe_allow_html=True)
+#st.write(piura_map._repr_html_(), unsafe_allow_html=True)
 
 import matplotlib.pyplot as plt
 # Filtra los datos de latitud y longitud para los distritos de Piura
